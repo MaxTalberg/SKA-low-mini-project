191ff50b7ae5a32eb2aee413c027c100002099be
Max Talberg <mt942@cam.ac.uk>
2024-03-12 21:46:30 +0000

    Updated stefcal optimised

--- harp_beam/functions.py
+++ harp_beam/functions.py
@@ -15,10 +15,10 @@ def to_dBV(magnitude):
     '''
     return 20*np.log10(magnitude)
 
-# StEFCal algorithm 1
-def stefcal1(max_iteration=100, threshold=1e-6):
+# StEFCal algorithm
+def stefcal(M, R, max_iteration=100, threshold=1e-6):
     # Initial gain matrix G
-    G = np.eye(len(g_sol)) # Identity matrix
+    G = np.eye(len(M)) # Identity matrix
 
     # Iterative loop
     for i in range(max_iteration):
@@ -26,7 +26,7 @@ def stefcal1(max_iteration=100, threshold=1e-6):
         G_prev = G.copy() 
 
         for p in range(G.shape[0]):  # Loop over antennas p
-            z = np.dot(G_prev, M_AEP[:, p])  # Use all rows of M_AEP for antenna p
+            z = np.dot(G_prev, M[:, p])  # Use all rows of M_AEP for antenna p
             gp = np.dot(np.conjugate(R[:, p]).T, z) / np.dot(np.conjugate(z).T, z)  # Calculate new gain for antenna p
             G[p, p] = gp  # Update the gain for antenna p in the matrix
 
@@ -36,7 +36,33 @@ def stefcal1(max_iteration=100, threshold=1e-6):
             if delta_G < threshold:
                 print(f"Convergence reached after {i+1} iterations.")
                 break
-        # Averaging step
-        G = (G + G_prev) / 2
+            else:
+                G = (G + G_prev) / 2
+
+    return G
+
+# implement optimised StEFCal algorithm
+def stefcal_optimised(M, R, max_iteration=100, threshold=1e-6):
+    # Initial gain matrix G
+    G = np.eye(len(M)) # Identity matrix
+
+    # Iterative loop
+    for i in range(max_iteration):
+        # Last iteration of G for comparison
+        G_prev = G.copy() 
+
+        for p in range(G.shape[0]):  # Loop over antennas p
+            z = np.dot(G, M[:, p])  # Use all rows of M_AEP for antenna p
+            gp = np.dot(np.conjugate(R[:, p]).T, z) / np.dot(np.conjugate(z).T, z)  # Calculate new gain for antenna p
+            G[p, p] = gp  # Update the gain for antenna p in the matrix
+
+        # Convergence check at every iteration
+        if np.linalg.norm(G - G_prev, 'fro') / np.linalg.norm(G, 'fro') < threshold:
+            print(f"Convergence reached after {i+1} iterations.")
+            break
+
+        # Averaging step only on even iterations
+        if i % 2 == 0:
+            G = (G + G_prev) / 2
 
     return G
