2831e1331fd0d0bbbb5bd4314087692d847007ea
Max Talberg <mt942@cam.ac.uk>
2024-03-12 13:30:30 +0000

    Updated main.py for Q2

--- harp_beam/main.py
+++ harp_beam/main.py
@@ -11,7 +11,7 @@ num_dir = 256
 theta = np.linspace(-np.pi/2, np.pi/2, num_dir)
 phi = np.zeros_like(theta)
 
-#EEPs
+# Compute EEPs
 v_theta_polY, v_phi_polY, v_theta_polX, v_phi_polX = compute_EEPs(theta.copy()[:, None], phi.copy()[:, None])
 
 # Calculate magnitude of EEPs in dBV
@@ -21,7 +21,6 @@ magnitude_EEP_polX = to_dBV(np.abs(v_theta_polX))
 # Calculate AEPs
 AEP_polY = to_dBV(np.sqrt(np.mean(np.abs(v_theta_polY)**2, axis=1)))
 AEP_polX = to_dBV(np.sqrt(np.mean(np.abs(v_theta_polX)**2, axis=1)))
-
 
 # plot EEPs and AEPs
 plot2(theta, v_theta_polY, v_theta_polX, magnitude_EEP_polY, magnitude_EEP_polX, AEP_polY, AEP_polX)
